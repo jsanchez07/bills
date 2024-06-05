ca7cdb912b57321ffc00fe591c6dc087311493ba
Jesse Sanchez <jsanchez@adobe.com>
2024-06-05 16:04:58 -0500

    rearranging the list

--- lists/groceries/groceries.js
+++ lists/groceries/groceries.js
@@ -13,3 +13,30 @@ function removeFromList(){
     var ul = document.querySelector("#theList");
     ul.removeChild(ul.lastChild);
 }
+function listenToCheckbox() {
+    var checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                console.log('Checkbox is checked');
+                reArrangeList();
+            } else {
+                console.log('Checkbox is unchecked');
+            }
+        });
+    });
+}
+
+
+function reArrangeList(){
+    var ul = document.querySelector("#theList");
+    var items = ul.children;
+    for(var i = 0; i < items.length; i++){
+        if(items[i].firstChild.checked){
+            ul.appendChild(items[i]);
+        }
+        else if (!items[i].firstChild.checked){
+            ul.insertBefore(items[i], items[0]);
+        }
+    }
+}
