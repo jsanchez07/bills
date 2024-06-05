9dea5ff50f97e31652dd1081d952c1f653247e8e
Jesse Sanchez <jsanchez@adobe.com>
2024-06-05 15:58:04 -0500

    adding the checkbox

--- lists/groceries/groceries.js
+++ lists/groceries/groceries.js
@@ -1,6 +1,9 @@
 function addToList(newItem){
     var ul = document.querySelector("#theList");
     var li = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    li.appendChild(checkbox);
     li.appendChild(document.createTextNode(newItem));
     ul.appendChild(li);
     document.querySelector('#item').value = "";
