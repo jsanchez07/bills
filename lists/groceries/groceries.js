f53979280163dd53792f34b7b117b266762c4aa7
Jesse Sanchez <jsanchez@adobe.com>
2024-06-05 15:17:08 -0500

    using queryselector

--- lists/groceries/groceries.js
+++ lists/groceries/groceries.js
@@ -1,5 +1,5 @@
 function addToList(newItem){
-    var ul = document.getElementById("list");
+    var ul = document.querySelector(".list");
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(newItem));
     ul.appendChild(li);
