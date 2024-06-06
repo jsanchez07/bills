79dbebfaf99c539f0d9fcd0ea15a1f9d183dfc6e
Jesse Sanchez <jsanchez@adobe.com>
2024-06-06 15:11:34 -0500

    to see errors

--- lists/groceries/groceries.php
+++ lists/groceries/groceries.php
@@ -5,6 +5,8 @@
 </head>
 
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
     echo "I am here<br />";
     require('dbConfig.php');
     echo "localhost variable: ".$localhost."<br />";
