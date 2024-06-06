4956aca2cbba9d90cdc75b6fa54c008581e5b629
Jesse Sanchez <jsanchez@adobe.com>
2024-06-06 15:12:36 -0500

    message to turn on errors

--- lists/groceries/groceries.php
+++ lists/groceries/groceries.php
@@ -7,6 +7,7 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
+    echo "the errors are turned on<br />";
     echo "I am here<br />";
     require('dbConfig.php');
     echo "localhost variable: ".$localhost."<br />";
