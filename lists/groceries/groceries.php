28b65b24dd21b4c6201e3af7611556324eb34c78
Jesse Sanchez <jsanchez@adobe.com>
2024-06-06 22:44:35 -0500

    changing to have remove button

--- lists/groceries/groceries.php
+++ lists/groceries/groceries.php
@@ -63,10 +63,10 @@
                     for($i=0; $i<$num; $i++){
                         $row = mysqli_fetch_array($result);
                         if($row['isChecked'] == 1){
-                            echo "<li id = ".$row['item']."><input type='checkbox' checked>". $row['item']." category: ".$row['category']."<button id= 'remove' onclick='removeThisItem(".$row['item'].")'>Remove</button></li>";
+                            echo "<li id = ".$row['item']."><input type='checkbox' checked>". $row['item']." category: ".$row['category']."<button id= 'removeThis' onclick='removeThisItem(".$row['item'].")'>x</button></li>";
                         }
                         else{
-                            echo "<li id = ".$row['item']."><input type='checkbox'>". $row['item']." category: ".$row['category']."</li>";
+                            echo "<li id = ".$row['item']."><input type='checkbox'>". $row['item']." category: ".$row['category']."<button id= 'removeThis' onclick='removeThisItem(".$row['item'].")'>x</button></li>";
                         }
                     }
                     mysqli_close($con);
